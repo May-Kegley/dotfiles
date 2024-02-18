023f386843b003d24652d895cefc827020cfc677
May Kegley <may.kegley@proton.me>
2024-02-17 19:08:30 -0500

    update neotree conf

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem toggle left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers toggle float<CR>", {})
 	end,
 }
